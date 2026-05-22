ad8ad1375a07de9c52d71f69752925bb36bb96f1
Ethan Baedke <ethanbaedke@gmail.com>
2026-05-21 21:31:27 -0500

    Logger can now filter out messages from selected classes

--- race-down-roosevelt/autoloads/rdr_logger.gd
+++ race-down-roosevelt/autoloads/rdr_logger.gd
@@ -2,7 +2,14 @@ extends Node
 
 const SPAM_LOGGING_ENABLED:bool = false
 
+var _ignored_classes:Dictionary[String, bool] = {
+	(Race as GDScript).get_global_name(): true,
+}
+
 func log(source:Object, contents:String) -> void:
+	
+	if (_is_ignored(source)):
+		return
 	
 	var prefix:String = _get_prefix(source)
 	print(prefix + " " + contents)
@@ -12,16 +19,25 @@ func spam_log(source:Object, contents:String) -> void:
 
 	if (!SPAM_LOGGING_ENABLED):
 		return
+		
+	if (_is_ignored(source)):
+		return
 
 	var prefix:String = _get_prefix(source)
 	print(prefix + " " + contents)
 	
 func warn(source:Object, contents:String) -> void:
 	
+	if (_is_ignored(source)):
+		return
+	
 	var prefix:String = _get_prefix(source)
 	print_rich("[color=GOLDENROD]" + prefix + " " + contents + "[/color]")
 
 func error(source:Object, contents:String) -> void:
+	
+	if (_is_ignored(source)):
+		return
 	
 	var prefix:String = _get_prefix(source)
 	print_rich("[color=SALMON]" + prefix + " " + contents + "[/color]")
@@ -29,9 +45,17 @@ func error(source:Object, contents:String) -> void:
 # A fatal log will crash the game.
 func fatal(source:Object, contents:String) -> void:
 	
+	if (_is_ignored(source)):
+		return
+	
 	var prefix:String = _get_prefix(source)
 	print_rich("[color=FUCHSIA]" + prefix + " " + contents + "[/color]")
 	get_tree().quit(1)
+
+func _is_ignored(source:Object) -> bool:
+	
+	var script_type:String = source.get_script().get_global_name()
+	return _ignored_classes.has(script_type)
 
 func _get_prefix(source:Object) -> String:
 	

13654ffe68c4449dc8b66d22876a9da8d5c6cb58
Ethan Baedke <ethanbaedke@gmail.com>
2026-05-23 15:23:42 -0500

    Added different speeds to traffic vehicles and handeled traffic vehicles colliding with one another.

--- race-down-roosevelt/vehicles/traffic_vehicles/traffic_vehicle.gd
+++ race-down-roosevelt/vehicles/traffic_vehicles/traffic_vehicle.gd
@@ -1,6 +1,9 @@
 class_name TrafficVehicle extends Node3D
 
-@onready var collision_shape:CollisionShape3D = $Area3D/CollisionShape3D
+@export var speed:int = 1
+
+@onready var _collision_area:Area3D = $Area3D
+@onready var _collision_shape:CollisionShape3D = $Area3D/CollisionShape3D
 
 func explode() -> void:
 	
@@ -9,10 +12,36 @@ func explode() -> void:
 
 func disable_vehicle() -> void:
 	
-	collision_shape.disabled = true
+	_collision_shape.disabled = true
 	self.visible = false
 
 func enable_vehicle() -> void:
 	
-	collision_shape.disabled = false
+	_collision_shape.disabled = false
 	self.visible = true
+
+# Called when this racer bumps a racer in front of it.
+func _handle_traffic_vehicle_bump(other:TrafficVehicle) -> void:
+	
+	RdrLogger.log(self, "Traffic vehicle rear ended another traffic vehicle. Set to tailgate.")
+
+	# Simply set the behind vehicles speed equal to the bumped vehicles speed and let it tailgate.
+	self.speed = other.speed
+
+func _collision_area_entered(area: Area3D) -> void:
+	
+	var parent:Node3D = area.get_parent_node_3d()
+	if (parent is TrafficVehicle):
+		# Ignore if this collision if this vehicle is in front of the vehicle it hit.
+		# Traffic vehicle bumps are always handeled by the behind vehicle.
+		if (area.global_position.z < _collision_area.global_position.z):
+			return
+		_handle_traffic_vehicle_bump(parent)
+
+func _ready() -> void:
+	
+	_collision_area.area_entered.connect(_collision_area_entered)
+
+func _process(delta: float) -> void:
+	
+	position.z -= speed * delta

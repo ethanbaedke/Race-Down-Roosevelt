a59cf1deccb5c94770e6abef1847573e0fcfe49c
Ethan Baedke <110857520+ethanbaedke@users.noreply.github.com>
2026-07-03 12:11:36 -0500

    Fixed item row to recreate missing items on recycle.

--- race-down-roosevelt/road/item_row.gd
+++ race-down-roosevelt/road/item_row.gd
@@ -5,11 +5,33 @@ class_name ItemRow extends Node3D
 	preload("res://items/boost_item_data.tres")
 ]
 
+var _items:Array[Item] = []
+
+func _create_random_item() -> Item:
+	
+	var item:Item = _item_scene.instantiate()
+	var item_ind:int = randi_range(0, _item_data_resources.size() - 1)
+	item.item_data = _item_data_resources[item_ind]
+	return item
+
 func _ready() -> void:
 	
 	for i:int in range(-4, 5):
-		var item_ind:int = randi_range(0, _item_data_resources.size() - 1)
-		var item:Item = _item_scene.instantiate()
-		item.item_data = _item_data_resources[item_ind]
+		var item:Item = _create_random_item()
+		_items.append(item)
 		self.add_child(item)
 		item.position.x = i * 3
+		
+func _enter_tree() -> void:
+	
+	# Don't process this if _ready hasn't been called yet.
+	if (_items.size() != 9):
+		return
+	
+	# When we recycle this row, ensure any items that were previously taken are recreated.
+	for i:int in range(-4, 5):
+		if (_items[i + 4] == null):
+			var item:Item = _create_random_item()
+			_items[i + 4] = item
+			self.add_child(item)
+			item.position.x = i * 3

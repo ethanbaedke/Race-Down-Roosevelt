8a174714949dfbcec40210160d1b06646b09bf10
Ethan Baedke <110857520+ethanbaedke@users.noreply.github.com>
2026-07-11 19:57:43 -0500

    Disabling input glyphs when not usable.

--- race-down-roosevelt/race/race_player_hud.gd
+++ race-down-roosevelt/race/race_player_hud.gd
@@ -6,16 +6,19 @@ class_name RacePlayerHud extends Control
 @onready var _item_disabled_texture_rect:TextureRect = $MarginContainer/VBoxContainer/HBoxContainer/HBoxContainer/VBoxContainer/SizeFitterControl/ItemDisabledTexture
 @onready var _jump_disabled_texture_rect:TextureRect = $MarginContainer/VBoxContainer/HBoxContainer/HBoxContainer/VBoxContainer2/SizeFitterControl2/JumpDisabledTexture
 @onready var _name_label:Label = $MarginContainer/VBoxContainer/HBoxContainer/PanelContainer/NameLabel
-@onready var _item_input_glyph:InputGlyph = $MarginContainer/VBoxContainer/HBoxContainer/HBoxContainer/VBoxContainer/InputGlyph
-@onready var _jump_input_glyph:InputGlyph = $MarginContainer/VBoxContainer/HBoxContainer/HBoxContainer/VBoxContainer2/InputGlyph
+@onready var _item_input_glyph:InputGlyph = $MarginContainer/VBoxContainer/HBoxContainer/HBoxContainer/VBoxContainer/Control/InputGlyph
+@onready var _jump_input_glyph:InputGlyph = $MarginContainer/VBoxContainer/HBoxContainer/HBoxContainer/VBoxContainer2/Control/InputGlyph
 
 func update_item(data:ItemData) -> void:
 	
 	if (data == null):
 		_item_texture_rect.texture = null
+		_item_input_glyph.visible = false
 		return
 	
 	_item_texture_rect.texture = data.item_icon
+	if (_item_disabled_texture_rect.visible == false):
+		_item_input_glyph.visible = true
 
 func update_item_time(current_time:float, total_time:float) -> void:
 	
@@ -25,19 +28,36 @@ func update_item_time(current_time:float, total_time:float) -> void:
 	
 	_item_progress_bar.visible = true
 	_item_progress_bar.value = (current_time / total_time) * 100.0
+	_item_input_glyph.visible = false
 
 func update_gas(current_amount:float, max_amount:float) -> void:
 	
 	var percent:float = current_amount / max_amount
 	_jump_texture_rect.material.set_shader_parameter("percent", percent)
+	if (percent == 1.0 && _jump_disabled_texture_rect.visible == false):
+		_jump_input_glyph.visible = true
+	else:
+		_jump_input_glyph.visible = false
 
 func update_item_usable_state(usable:bool) -> void:
 	
-	_item_disabled_texture_rect.visible = !usable
+	if (usable && _item_disabled_texture_rect.visible == true):
+		_item_disabled_texture_rect.visible = false
+		if (_item_texture_rect.texture != null):
+			_item_input_glyph.visible = true
+	elif (!usable && _item_disabled_texture_rect.visible == false):
+		_item_disabled_texture_rect.visible = true
+		_item_input_glyph.visible = false
 
 func update_gas_usable_state(usable:bool) -> void:
 	
-	_jump_disabled_texture_rect.visible = !usable
+	if (usable && _jump_disabled_texture_rect.visible == true):
+		_jump_disabled_texture_rect.visible = false
+		if (_jump_texture_rect.material.get_shader_parameter("percent") == 1.0):
+			_jump_input_glyph.visible = true
+	elif (!usable && _jump_disabled_texture_rect.visible == false):
+		_jump_disabled_texture_rect.visible = true
+		_jump_input_glyph.visible = false
 
 func update_name(new_name:String) -> void:
 	
@@ -47,3 +67,8 @@ func update_device_type(device_type:Globals.DeviceType) -> void:
 	
 	_item_input_glyph.set_glyph(device_type, InputGlyph.ActionType.ITEM)
 	_jump_input_glyph.set_glyph(device_type, InputGlyph.ActionType.JUMP)
+
+func _ready() -> void:
+	
+	_item_input_glyph.visible = false
+	_jump_input_glyph.visible = false

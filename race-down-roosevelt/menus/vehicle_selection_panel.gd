458ffefcdb99b55359496d7a39ed0fec6cc6441f
Ethan Baedke <110857520+ethanbaedke@users.noreply.github.com>
2026-08-09 15:50:07 -0500

    Fixed wrong vehicles loaded after selection.

--- race-down-roosevelt/menus/vehicle_selection_panel.gd
+++ race-down-roosevelt/menus/vehicle_selection_panel.gd
@@ -1,11 +1,11 @@
 class_name VehicleSelectionPanel extends Control
 
 const VEHICLE_DATA:Array[RacerVehicleData] = [
-	preload("res://vehicles/racer_vehicles/honda_civic_data.tres"),
-	preload("res://vehicles/racer_vehicles/subaru_forester_data.tres"),
 	preload("res://vehicles/racer_vehicles/jeep_wrangler_data.tres"),
-	preload("res://vehicles/racer_vehicles/cybertruck_data.tres"),
-	preload("res://vehicles/racer_vehicles/mini_cooper_data.tres")
+	preload("res://vehicles/racer_vehicles/subaru_forester_data.tres"),
+	preload("res://vehicles/racer_vehicles/mini_cooper_data.tres"),
+	preload("res://vehicles/racer_vehicles/honda_civic_data.tres"),
+	preload("res://vehicles/racer_vehicles/cybertruck_data.tres")
 ]
 
 enum PanelState {
